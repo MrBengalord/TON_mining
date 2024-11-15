f966f48cc90449674d1acc01cb30daeec1a65cfd
MrBengalord <novozhenov.dmitrii@gmail.com>
2024-11-15 15:08:49 +0300

    Update mine.ts

---
+++ scripts/mine.ts
@@ -0,0 +1,11 @@
+import {Address} from '@ton/ton';
+
+const walletAddress = Address.parse('0QDixH9HdKCqIVoY9uU1HUOk46PV2RhlxJ5EsXOSJ3dikYMC');
+const collectionAddress = Address.parse('EQDk8N7xM5D669LC2YACrseBJtDyFqwtSPCNhRWXU7kjEptX');
+
+async function mine () {
+
+
+}
+
+mine();
